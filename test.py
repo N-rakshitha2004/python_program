24b7b6e68418e1f0892af9baba9dc4925eb1d45f
N-rakshitha2004 <nrzkshitha787@gmail.com>
2024-06-05 11:48:39 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,85 @@
+
+print("hello world")
+
+# int
+num = 10
+print(10)
+
+# float
+num =10.0
+print(num)
+
+
+#boolean
+is_developer = True
+
+#str
+name = "rakshitha"
+print(name)
+
+# list
+nums = [1,2,3,4,5]
+print(type(nums))
+
+#tuple
+nums =(1,2,3,4,5)
+print(type(nums))
+
+#set
+nums = {1,2,3,4,5}
+print(type(nums))
+
+#dict
+info = {"name": "rakshitha", "email": "n.rakshitha@gmail.com"}
+print(type(info))
+
+print(info)
+
+#none
+name = None
+print(type(name))
+
+
+
+
+print("hello world")
+print(50+50)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
